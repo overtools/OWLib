4db7567127ecb28b1926c9f583ccecdde0018b70
Naomi <yukimono6@gmail.com>
2016-06-03 04:24:35 +0900

    TextureTool restructure args, now supports linear 004 textures (kind of)

---
+++ OWLib/TextureLinear.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using OWLib.Types;
+
+namespace OWLib {
+  public class TextureLinear {
+    private TextureHeader header;
+    private DXGI_PIXEL_FORMAT format;
+    private uint size;
+    private ulong[] color;
+    private bool loaded = false;
+
+    public TextureHeader Header => header;
+    public DXGI_PIXEL_FORMAT Format => format;
+    public uint Size => size;
+    public ulong[] Color => color;
+    public bool Loaded => loaded;
+
+    public enum TEXTURE_STYLE : byte {
+      UNKNOWN = 0,
+      TYPELESS = 1,
+      FLOAT = 2,
+      UNSIGNED = 3,
+      SIGNED = 4
+    };
+
+    public TextureLinear(Stream imageStream) {
+      using(BinaryReader imageReader = new BinaryReader(imageStream)) {
+        header = imageReader.Read<TextureHeader>();
+        if(header.dataSize == 0) {
+          return;
+        }
+        size = header.dataSize;
+        format = header.format;
+
+        uint[] bits = new uint[4] { 0, 0, 0, 0 };
+        TEXTURE_STYLE style = 0;
+
+        if(format >= DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32A32_TYPELESS && format <= DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32A32_SINT) {
+          bits[0] = bits[1] = bits[2] = bits[3] = 32;
+        } else if(format >= DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32_TYPELESS && format <= DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32_SINT) {
+          bits[0] = bits[1] = bits[2] = 32;
+        } else if(format >= DXGI_PIXEL_FORMAT.DXGI_FORMAT_R16G16B16A16_TYPELESS && format <= DXGI_PIXEL_FORMAT.DXGI_FORMAT_R16G16B16A16_SINT) {
+          bits[0] = bits[1] = bits[2] = bits[3] = 16;
+        } else if(format >= DXGI_PIXEL_FORMAT.DXGI_FORMAT_R8G8B8A8_TYPELESS && format <= DXGI_PIXEL_FORMAT.DXGI_FORMAT_R8G8B8A8_SINT) {
+          bits[0] = bits[1] = bits[2] = bits[3] = 8;
+        } else {
+          return;
+        }
+
+        switch(format) {
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32A32_TYPELESS:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32_TYPELESS:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R16G16B16A16_TYPELESS:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R8G8B8A8_TYPELESS:
+            style = TEXTURE_STYLE.TYPELESS;
+            break;
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32A32_FLOAT:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32_FLOAT:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R16G16B16A16_FLOAT:
+            style = TEXTURE_STYLE.FLOAT;
+            break;
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32A32_UINT:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32_UINT:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R16G16B16A16_UINT:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R8G8B8A8_UINT:
+            style = TEXTURE_STYLE.UNSIGNED;
+            break;
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32A32_SINT:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R32G32B32_SINT:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R16G16B16A16_SINT:
+          case DXGI_PIXEL_FORMAT.DXGI_FORMAT_R8G8B8A8_SINT:
+            style = TEXTURE_STYLE.SIGNED;
+            break;
+        }
+
+        if(style == TEXTURE_STYLE.UNKNOWN) {
+          return;
+        }
+
+        imageStream.Seek(128, SeekOrigin.Begin);
+        uint pixels = (uint)(header.width * header.height);
+        color = new ulong[pixels];
+
+        for(int i = 0; i < pixels; ++i) {
+          ulong pixel = 0;
+          for(int j = 0; j < 4; ++j) {
+            uint bit = bits[j];
+            ushort v;
+            if(bit == 8) {
+              byte value = imageReader.ReadByte();
+              v = (ushort)((value / byte.MaxValue) * ushort.MaxValue);
+            } else if(bit == 16) {
+              ushort value = imageReader.ReadUInt16();
+              if(style == TEXTURE_STYLE.FLOAT) {
+                v = (ushort)((float)Half.ToHalf(value) * ushort.MaxValue);
+              } else {
+                v = value;
+              }
+            } else if(bit == 32) {
+              if(style == TEXTURE_STYLE.FLOAT) {
+                v = (ushort)(imageReader.ReadSingle() * ushort.MaxValue);
+              } else {
+                v = (ushort)((imageReader.ReadUInt32() / uint.MaxValue) * ushort.MaxValue);
+              }
+            } else {
+              v = ushort.MaxValue;
+            }
+            ulong x = (ulong)v << (j * 12);
+            pixel += x;
+          }
+          color[i] = pixel;
+        }
+      }
+      loaded = true;
+    }
+  }
+}
--- TextureTool/Program.cs
+++ TextureTool/Program.cs
@@ -2,28 +2,66 @@
 using System.IO;
 using OWLib;
 using System.Reflection;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace TextureTool {
   class Program {
     static void Main(string[] args) {
       if(args.Length < 3) {
-        Console.Out.WriteLine("Usage: TextureTool.exe 004_file 04D_file output_file");
+        Console.Out.WriteLine("Usage: TextureTool.exe output_file 2 004_file 04D_file");
+        Console.Out.WriteLine("Usage: TextureTool.exe output_file 1 004_file");
         return;
       }
 
       Console.Out.WriteLine("{0} v{1}", Assembly.GetExecutingAssembly().GetName().Name, Assembly.GetExecutingAssembly().GetName().Version.ToString());
 
-      string headerFile = args[0];
-      string dataFile = args[1];
-      string destFile = args[2];
+      string destFile = args[0];
 
-      using(Stream headerStream = File.Open(headerFile, FileMode.Open, FileAccess.Read))
-      using(Stream dataStream = File.Open(dataFile, FileMode.Open, FileAccess.Read)) {
-        Texture tex = new Texture(headerStream, dataStream);
-        Console.Out.WriteLine("Opened Texture. W: {0} H: {1}", tex.Header.width, tex.Header.height);
-        using(Stream ddsStream = File.Open(destFile, FileMode.OpenOrCreate, FileAccess.Write)) {
-          tex.ToDDS(ddsStream);
-          Console.Out.WriteLine("Saved DDS");
+      char mode = args[1][0];
+      string f004 = args[2];
+      string f04d = null;
+      if(mode == '2') {
+        f04d = args[3];
+      }
+
+      using(Stream headerStream = File.Open(f004, FileMode.Open, FileAccess.Read)) {
+        if(mode == '1') {
+          TextureLinear tex = new TextureLinear(headerStream);
+          Console.Out.WriteLine("Opened Texture. W: {0} H: {1} F: {2}", tex.Header.width, tex.Header.height, tex.Format.ToString());
+          if(tex.Loaded == false) {
+            Console.Error.WriteLine("Error?! (Probably unsupported format)");
+            return;
+          }
+          using(Stream pngStream = File.Open(destFile, FileMode.OpenOrCreate, FileAccess.Write)) {
+            Bitmap bmp = new Bitmap(tex.Header.width, tex.Header.height, PixelFormat.Format64bppPArgb);
+            BitmapData bits = bmp.LockBits(new Rectangle(0, 0, tex.Header.width, tex.Header.height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+            unsafe
+            {
+              for(int y = 0; y < tex.Header.height; ++y) {
+                ulong* row = (ulong*)((byte*)bits.Scan0 + (y * bits.Stride));
+                for(int x = 0; x < tex.Header.width; x++) {
+                  row[x] = tex.Color[x + (y * x)];
+                }
+              }
+            }
+            bmp.Save(pngStream, ImageFormat.Png);
+            Console.Out.WriteLine("Saved PNG");
+          }
+          return;
+        } else if(mode == '2') {
+          using(Stream dataStream = File.Open(f04d, FileMode.Open, FileAccess.Read)) {
+            Texture tex = new Texture(headerStream, dataStream);
+            Console.Out.WriteLine("Opened Texture. W: {0} H: {1}", tex.Header.width, tex.Header.height);
+            if(tex.Loaded == false) {
+              Console.Error.WriteLine("Error?! (Probably unsupported format)");
+              return;
+            }
+            using(Stream ddsStream = File.Open(destFile, FileMode.OpenOrCreate, FileAccess.Write)) {
+              tex.ToDDS(ddsStream);
+              Console.Out.WriteLine("Saved DDS");
+            }
+          }
         }
       }
     }
