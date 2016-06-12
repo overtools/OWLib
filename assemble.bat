96fc429d7c8d68309ec34eef3d54434accaed04f
Naomi <yukimono6@gmail.com>
2016-06-13 02:57:22 +0900

    Noesis script

--- assemble.bat
+++ assemble.bat
@@ -1,3 +1,4 @@
 git submodule update --init
 copy /Y /V CascLib.patch\Logger.cs CASCExplorer\CascLib\Logger.cs
 copy /Y /V CascLib.patch\PerfCounter.cs CASCExplorer\CascLib\PerfCounter.cs
+copy /Y /V Fbx.patch\FbxNode.cs FbxWriter\Fbx\FbxNode.cs
--- assemble.sh
+++ assemble.sh
@@ -5,3 +5,4 @@ git reset --hard HEAD
 popd
 patch --no-backup-if-mismatch -fsr /dev/null --verbose CASCExplorer/CascLib/Logger.cs CascLib.patch/Logger.cs.diff || cp -f CascLib.patch/Logger.cs CASCExplorer/CascLib/Logger.cs
 patch --no-backup-if-mismatch -fsr /dev/null --verbose CASCExplorer/CascLib/PerfCounter.cs CascLib.patch/PerfCounter.cs.diff || cp -f CascLib.patch/PerfCounter.cs CASCExplorer/CascLib/PerfCounter.cs
+patch --no-backup-if-mismatch -fsr /dev/null --verbose FbxWriter/Fbx/FbxNode.cs Fbx.patch/FbxNode.cs.diff || cp -f Fbx.patch/FbxNode.cs FbxWriter/Fbx/FbxNode.cs
