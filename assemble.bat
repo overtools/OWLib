bf7e5c01e4d63c9f8fbe8f9a4fec7d7164caa856
Naomi <yukimono6@gmail.com>
2016-06-14 07:06:01 +0900

    Prepare for source publish

--- assemble.bat
+++ assemble.bat
@@ -1,4 +1,3 @@
-git submodule update --init
 copy /Y /V CascLib.patch\Logger.cs CASCExplorer\CascLib\Logger.cs
 copy /Y /V CascLib.patch\PerfCounter.cs CASCExplorer\CascLib\PerfCounter.cs
 copy /Y /V Fbx.patch\FbxNode.cs FbxWriter\Fbx\FbxNode.cs
