e54a9315d00a0ad403f99bed0136d494ac7f57b2
Naomi <yukimono6@gmail.com>
2016-06-30 06:39:52 +0900

    Drop FBX

--- assemble.bat
+++ assemble.bat
@@ -1,4 +1,3 @@
 copy /Y /V CascLib.patch\Logger.cs CASCExplorer\CascLib\Logger.cs
 copy /Y /V CascLib.patch\PerfCounter.cs CASCExplorer\CascLib\PerfCounter.cs
 copy /Y /V CascLib.patch\OWRootHandler.cs CASCExplorer\CascLib\OWRootHandler.cs
-copy /Y /V Fbx.patch\FbxNode.cs FbxWriter\Fbx\FbxNode.cs
