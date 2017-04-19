5ed46b540b70c944aafbeff0a3a079ca25a96c9b
yukimono <yukimono6@gmail.com>
2017-04-19 20:05:09 +0100

    Fix CascLib from downloading files?

--- assemble.bat
+++ assemble.bat
@@ -3,4 +3,5 @@ copy /Y /V CascLib.patch\PerfCounter.cs CASCExplorer\CascLib\PerfCounter.cs
 copy /Y /V CascLib.patch\OWRootHandler.cs CASCExplorer\CascLib\OWRootHandler.cs
 copy /Y /V CascLib.patch\KeyService.cs CASCExplorer\CascLib\KeyService.cs
 copy /Y /V CascLib.patch\CASCConfig.cs CASCExplorer\CascLib\CASCConfig.cs
+copy /Y /V CascLib.patch\Extensions.cs CASCExplorer\CascLib\Extensions.cs
 
