a25992b96f468066bf9d8cb4a30f86599b6a4984
Kjasi <sephiroth3d@gmail.com>
2017-04-20 10:52:00 -0700

    -Fixed a bug that would rotate all Vec4s and Vec4ds 180 degrees by default. -Added more functionality to the Vec3/4 functions. -Fixed the indentation with new files

--- OWLib/Animation.cs
+++ OWLib/Animation.cs
@@ -5,200 +5,200 @@ using System.Text;
 using OWLib.Types;
 
 namespace OWLib {
-  public class Animation {
-    // Public items, for writing
-    public string Name;                 // The name of the animation. If no name can be provided, use the filename.
-    public float Duration;              // How long the animtion is, in seconds.
-    public float FramesPerSecond;       // The Number of frames per second. Duration * FPS = Total number of frames.
-    public List<Keyframe> Animations;
+    public class Animation {
+        // Public items, for writing
+        public string Name;                 // The name of the animation. If no name can be provided, use the filename.
+        public float Duration;              // How long the animtion is, in seconds.
+        public float FramesPerSecond;       // The Number of frames per second. Duration * FPS = Total number of frames.
+        public List<Keyframe> Animations;
 
-    // Private Items
-    private AnimHeader header;
-    private List<int> BoneList = new List<int>();
-    private List<AnimInfoTable> InfoTables = new List<AnimInfoTable>();
-    private int InfoTableSize = 0;
+        // Private Items
+        private AnimHeader header;
+        private List<int> BoneList = new List<int>();
+        private List<AnimInfoTable> InfoTables = new List<AnimInfoTable>();
+        private int InfoTableSize = 0;
 
-    private Vec4d UnpackRotation(ushort a, ushort b, ushort c) {
-      Vec4d q = new Vec4d();
-      int axis1 = a >> 15;
-      int axis2 = b >> 15;
-      int axis = axis1 << 1 | axis2;
-      axis = axis2 * 2 + axis1;
+        private Vec4d UnpackRotation(ushort a, ushort b, ushort c) {
+            Vec4d q = new Vec4d();
+            int axis1 = a >> 15;
+            int axis2 = b >> 15;
+            int axis = axis1 << 1 | axis2;
+            axis = axis2 * 2 + axis1;
 
-      a = (ushort)(a & 0x7FFF);
-      b = (ushort)(b & 0x7FFF);
+            a = (ushort)(a & 0x7FFF);
+            b = (ushort)(b & 0x7FFF);
 
-      double x, y, z, w;
-      x = 1.41421 * (a - 0x4000) / 0x8000;
-      y = 1.41421 * (b - 0x4000) / 0x8000;
-      z = 1.41421 * (c - 0x8000) / 0x10000;
-      w = Math.Pow(1.0 - x * x - y * y - z * z, 0.5);
+            double x, y, z, w;
+            x = 1.41421 * (a - 0x4000) / 0x8000;
+            y = 1.41421 * (b - 0x4000) / 0x8000;
+            z = 1.41421 * (c - 0x8000) / 0x10000;
+            w = Math.Pow(1.0 - x * x - y * y - z * z, 0.5);
 
-      Console.Out.WriteLine("Unpack Values: X: {0}, Y: {1}, Z: {2}, W: {3}, Axis: {4}", x, y, z, w, axis);
+            Console.Out.WriteLine("Unpack Values: X: {0}, Y: {1}, Z: {2}, W: {3}, Axis: {4}", x, y, z, w, axis);
 
-      if (axis == 0) {
-        q = new Vec4d(w, x, y, z);
-      } else if (axis == 1) {
-        q = new Vec4d(x, w, y, z);
-      } else if (axis == 2) {
-        q = new Vec4d(x, y, w, z);
-      } else if (axis == 3) {
-        q = new Vec4d(x, y, z, w);
-      } else {
-        Console.Out.WriteLine("Unknown Axis detected! Axis: %s", axis);
-      }
+            if (axis == 0) {
+                q = new Vec4d(w, x, y, z);
+            } else if (axis == 1) {
+                q = new Vec4d(x, w, y, z);
+            } else if (axis == 2) {
+                q = new Vec4d(x, y, w, z);
+            } else if (axis == 3) {
+                q = new Vec4d(x, y, z, w);
+            } else {
+                Console.Out.WriteLine("Unknown Axis detected! Axis: %s", axis);
+            }
 
-      return q;
-    }
-    private Vec3d UnpackScale(ushort x, ushort y, ushort z) {
-      double xd = (double)x / 1024.0;
-      double yd = (double)y / 1024.0;
-      double zd = (double)z / 1024.0;
+            return q;
+        }
+        private Vec3d UnpackScale(ushort x, ushort y, ushort z) {
+            double xd = (double)x / 1024.0;
+            double yd = (double)y / 1024.0;
+            double zd = (double)z / 1024.0;
 
-      Vec3d value = new Vec3d(xd, yd, zd);
-      return value;
-    }
-
-    public Animation(Stream animStream, string userName = "", bool leaveOpen = true) {
-      Name = animStream.ToString();
-      if (userName != "") {
-        Name = userName;
-      }
-      Animations = new List<Keyframe>();
-      // Convert OW Animation to our Animation Type
-      using (BinaryReader animReader = new BinaryReader(animStream, Encoding.Default, leaveOpen)) {
-        header = animReader.Read<AnimHeader>();
-
-        Duration = header.duration;
-        FramesPerSecond = header.fps;
-        InfoTableSize = (int)(header.fps * header.duration) + 1;
-        uint bonecount = header.bonecount;
-
-        animStream.Seek((long)header.boneListOffset, SeekOrigin.Begin);
-        for (uint i = 0; i < header.bonecount; i++) {
-          int boneID = animReader.ReadInt32();
-          BoneList.Add(boneID);
+            Vec3d value = new Vec3d(xd, yd, zd);
+            return value;
         }
 
-        Vec3d[,] ScaleValues = new Vec3d[bonecount, InfoTableSize];
-        Vec3d[,] PositionValues = new Vec3d[bonecount, InfoTableSize];
-        Vec4d[,] RotationValues = new Vec4d[bonecount, InfoTableSize];
-        bool[,] hasScale = new bool[bonecount, InfoTableSize];
-        bool[,] hasPosition = new bool[bonecount, InfoTableSize];
-        bool[,] hasRotation = new bool[bonecount, InfoTableSize];
-
-        animStream.Seek((long)header.infoTableOffset, SeekOrigin.Begin);
-        for (int boneid = 0; boneid < header.bonecount; boneid++) {
-          long animStreamPos = animStream.Position;
-          AnimInfoTable it = animReader.Read<AnimInfoTable>();
-          long SIO = (long)it.ScaleIndicesOffset * 4 + animStreamPos;
-          long PIO = (long)it.PositionIndicesOffset * 4 + animStreamPos;
-          long RIO = (long)it.RotationIndicesOffset * 4 + animStreamPos;
-          long SDO = (long)it.ScaleDataOffset * 4 + animStreamPos;
-          long PDO = (long)it.PositionDataOffset * 4 + animStreamPos;
-          long RDO = (long)it.RotationDataOffset * 4 + animStreamPos;
-          InfoTables.Add(it);
-
-          // Read Indices
-          List<int> ScaleIndexList = new List<int>();
-          animStream.Seek(SIO, SeekOrigin.Begin);
-          for (int j = 0; j < it.ScaleCount; j++) {
-            if (InfoTableSize < 255) {
-              ScaleIndexList.Add((int)animReader.ReadByte());
-            } else {
-              ScaleIndexList.Add((int)animReader.ReadInt16());
+        public Animation(Stream animStream, string userName = "", bool leaveOpen = true) {
+            Name = animStream.ToString();
+            if (userName != "") {
+                Name = userName;
             }
-          }
-          List<int> PositonIndexList = new List<int>();
-          animStream.Seek(PIO, SeekOrigin.Begin);
-          for (int j = 0; j < it.PositionCount; j++) {
-            if (InfoTableSize < 255) {
-              PositonIndexList.Add((int)animReader.ReadByte());
-            } else {
-              PositonIndexList.Add((int)animReader.ReadInt16());
+            Animations = new List<Keyframe>();
+            // Convert OW Animation to our Animation Type
+            using (BinaryReader animReader = new BinaryReader(animStream, Encoding.Default, leaveOpen)) {
+                header = animReader.Read<AnimHeader>();
+
+                Duration = header.duration;
+                FramesPerSecond = header.fps;
+                InfoTableSize = (int)(header.fps * header.duration) + 1;
+                uint bonecount = header.bonecount;
+
+                animStream.Seek((long)header.boneListOffset, SeekOrigin.Begin);
+                for (uint i = 0; i < header.bonecount; i++) {
+                    int boneID = animReader.ReadInt32();
+                    BoneList.Add(boneID);
+                }
+
+                Vec3d[,] ScaleValues = new Vec3d[bonecount, InfoTableSize];
+                Vec3d[,] PositionValues = new Vec3d[bonecount, InfoTableSize];
+                Vec4d[,] RotationValues = new Vec4d[bonecount, InfoTableSize];
+                bool[,] hasScale = new bool[bonecount, InfoTableSize];
+                bool[,] hasPosition = new bool[bonecount, InfoTableSize];
+                bool[,] hasRotation = new bool[bonecount, InfoTableSize];
+
+                animStream.Seek((long)header.infoTableOffset, SeekOrigin.Begin);
+                for (int boneid = 0; boneid < header.bonecount; boneid++) {
+                    long animStreamPos = animStream.Position;
+                    AnimInfoTable it = animReader.Read<AnimInfoTable>();
+                    long SIO = (long)it.ScaleIndicesOffset * 4 + animStreamPos;
+                    long PIO = (long)it.PositionIndicesOffset * 4 + animStreamPos;
+                    long RIO = (long)it.RotationIndicesOffset * 4 + animStreamPos;
+                    long SDO = (long)it.ScaleDataOffset * 4 + animStreamPos;
+                    long PDO = (long)it.PositionDataOffset * 4 + animStreamPos;
+                    long RDO = (long)it.RotationDataOffset * 4 + animStreamPos;
+                    InfoTables.Add(it);
+
+                    // Read Indices
+                    List<int> ScaleIndexList = new List<int>();
+                    animStream.Seek(SIO, SeekOrigin.Begin);
+                    for (int j = 0; j < it.ScaleCount; j++) {
+                        if (InfoTableSize < 255) {
+                            ScaleIndexList.Add((int)animReader.ReadByte());
+                        } else {
+                            ScaleIndexList.Add((int)animReader.ReadInt16());
+                        }
+                    }
+                    List<int> PositonIndexList = new List<int>();
+                    animStream.Seek(PIO, SeekOrigin.Begin);
+                    for (int j = 0; j < it.PositionCount; j++) {
+                        if (InfoTableSize < 255) {
+                            PositonIndexList.Add((int)animReader.ReadByte());
+                        } else {
+                            PositonIndexList.Add((int)animReader.ReadInt16());
+                        }
+                    }
+                    List<int> RotationIndexList = new List<int>();
+                    animStream.Seek(RIO, SeekOrigin.Begin);
+                    for (int j = 0; j < it.RotationCount; j++) {
+                        if (InfoTableSize < 255) {
+                            RotationIndexList.Add((int)animReader.ReadByte());
+                        } else {
+                            RotationIndexList.Add((int)animReader.ReadInt16());
+                        }
+                    }
+                    // Read Data
+                    animStream.Seek(SDO, SeekOrigin.Begin);
+                    for (int j = 0; j < it.ScaleCount; j++) {
+                        int Index = ScaleIndexList[j];
+                        hasScale[boneid, Index] = true;
+                        ushort x = animReader.ReadUInt16();
+                        ushort y = animReader.ReadUInt16();
+                        ushort z = animReader.ReadUInt16();
+
+                        Vec3d values = UnpackScale(x, y, z);
+                        ScaleValues[boneid, Index] = values;
+                    }
+                    animStream.Seek(PDO, SeekOrigin.Begin);
+                    for (int j = 0; j < it.PositionCount; j++) {
+                        int Index = PositonIndexList[j];
+                        hasPosition[boneid, Index] = true;
+                        float x = animReader.ReadSingle();
+                        float y = animReader.ReadSingle();
+                        float z = animReader.ReadSingle();
+
+                        Vec3d values = new Vec3d(x, y, z);
+                        PositionValues[boneid, Index] = values;
+                    }
+                    animStream.Seek(RDO, SeekOrigin.Begin);
+                    for (int j = 0; j < it.RotationCount; j++) {
+                        int Index = RotationIndexList[j];
+                        hasRotation[boneid, Index] = true;
+                        ushort x = animReader.ReadUInt16();
+                        ushort y = animReader.ReadUInt16();
+                        ushort z = animReader.ReadUInt16();
+
+                        Vec4d values = UnpackRotation(x, y, z);
+                        RotationValues[boneid, Index] = values;
+                    }
+                    animStream.Seek(animStreamPos + 32L, SeekOrigin.Begin);
+                }
+
+
+                for (int frame = 0; frame < InfoTableSize; frame++) {
+                    Keyframe kf = new Keyframe();
+                    kf.FramePosition = ((float)frame / FramesPerSecond);
+                    kf.BoneFrames = new List<BoneAnimation>();
+                    for (int bone = 0; bone < header.bonecount; bone++) {
+                        // Build Value Data
+                        BoneAnimation ba = new BoneAnimation();
+                        ba.BoneID = BoneList[bone];
+                        ba.Values = new List<FrameValue>();
+
+                        if (hasScale[bone, frame]) {
+                            Vec3d v = ScaleValues[bone, frame];
+                            FrameValue fv = new FrameValue(AnimChannelID.SCALE, v);
+                            ba.Values.Add(fv);
+                        }
+                        if (hasPosition[bone, frame]) {
+                            Vec3d v = PositionValues[bone, frame];
+                            FrameValue f = new FrameValue(AnimChannelID.POSITION, v);
+                            ba.Values.Add(f);
+                        }
+                        if (hasRotation[bone, frame]) {
+                            Vec4d v = RotationValues[bone, frame];
+                            FrameValue f = new FrameValue(AnimChannelID.ROTATION, v);
+                            ba.Values.Add(f);
+                        }
+                        if (ba.Values.Count > 0) {
+                            kf.BoneFrames.Add(ba);
+                        }
+                    }
+                    if (kf.BoneFrames.Count > 0) {
+                        Animations.Add(kf);
+                    }
+                }
             }
-          }
-          List<int> RotationIndexList = new List<int>();
-          animStream.Seek(RIO, SeekOrigin.Begin);
-          for (int j = 0; j < it.RotationCount; j++) {
-            if (InfoTableSize < 255) {
-              RotationIndexList.Add((int)animReader.ReadByte());
-            } else {
-              RotationIndexList.Add((int)animReader.ReadInt16());
-            }
-          }
-          // Read Data
-          animStream.Seek(SDO, SeekOrigin.Begin);
-          for (int j = 0; j < it.ScaleCount; j++) {
-            int Index = ScaleIndexList[j];
-            hasScale[boneid, Index] = true;
-            ushort x = animReader.ReadUInt16();
-            ushort y = animReader.ReadUInt16();
-            ushort z = animReader.ReadUInt16();
-
-            Vec3d values = UnpackScale(x, y, z);
-            ScaleValues[boneid, Index] = values;
-          }
-          animStream.Seek(PDO, SeekOrigin.Begin);
-          for (int j = 0; j < it.PositionCount; j++) {
-            int Index = PositonIndexList[j];
-            hasPosition[boneid, Index] = true;
-            float x = animReader.ReadSingle();
-            float y = animReader.ReadSingle();
-            float z = animReader.ReadSingle();
-
-            Vec3d values = new Vec3d(x, y, z);
-            PositionValues[boneid, Index] = values;
-          }
-          animStream.Seek(RDO, SeekOrigin.Begin);
-          for (int j = 0; j < it.RotationCount; j++) {
-            int Index = RotationIndexList[j];
-            hasRotation[boneid, Index] = true;
-            ushort x = animReader.ReadUInt16();
-            ushort y = animReader.ReadUInt16();
-            ushort z = animReader.ReadUInt16();
-
-            Vec4d values = UnpackRotation(x, y, z);
-            RotationValues[boneid, Index] = values;
-          }
-          animStream.Seek(animStreamPos + 32L, SeekOrigin.Begin);
         }
-
-
-        for (int frame = 0; frame < InfoTableSize; frame++) {
-          Keyframe kf = new Keyframe();
-          kf.FramePosition = ((float)frame / FramesPerSecond);
-          kf.BoneFrames = new List<BoneAnimation>();
-          for (int bone = 0; bone < header.bonecount; bone++) {
-            // Build Value Data
-            BoneAnimation ba = new BoneAnimation();
-            ba.BoneID = BoneList[bone];
-            ba.Values = new List<FrameValue>();
-            
-            if (hasScale[bone, frame]) {
-              Vec3d v = ScaleValues[bone, frame];
-              FrameValue fv = new FrameValue(AnimChannelID.SCALE, v);
-              ba.Values.Add(fv);
-            }
-            if (hasPosition[bone, frame]) {
-              Vec3d v = PositionValues[bone, frame];
-              FrameValue f = new FrameValue(AnimChannelID.POSITION, v);
-              ba.Values.Add(f);
-            }
-            if (hasRotation[bone, frame]) {
-              Vec4d v = RotationValues[bone, frame];
-              FrameValue f = new FrameValue(AnimChannelID.ROTATION, v);
-              ba.Values.Add(f);
-            }
-            if (ba.Values.Count > 0) {
-              kf.BoneFrames.Add(ba);
-            }
-          }
-          if (kf.BoneFrames.Count > 0) {
-            Animations.Add(kf);
-          }
-        }
-      }
     }
-  }
 }
--- OWLib/Types/CommonTypes.cs
+++ OWLib/Types/CommonTypes.cs
@@ -55,31 +55,21 @@ namespace OWLib.Types {
     }
 
     [StructLayout(LayoutKind.Sequential, Pack = 4)]
-    public struct Vec4 {
-        public float x;
-        public float y;
-        public float z;
-        public float w;
-    }
-
-
-    [StructLayout(LayoutKind.Sequential, Pack = 4)]
-    public struct Vec4d {
-        public double x;
-        public double y;
-        public double z;
-        public double w;
-
-        public Vec4d(double x, double y, double z, double w = 1.0) {
-            this.x = x; this.y = y; this.z = z; this.w = w;
-        }
-    }
-
-    [StructLayout(LayoutKind.Sequential, Pack = 4)]
     public struct Vec3 {
         public float x;
         public float y;
         public float z;
+
+        public Vec3(float ix, float iy, float iz) {
+            x = ix;
+            y = iy;
+            z = iz;
+        }
+        public Vec3(double ix, double iy, double iz) {
+            x = (float)ix;
+            y = (float)iy;
+            z = (float)iz;
+        }
     }
 
     [StructLayout(LayoutKind.Sequential, Pack = 4)]
@@ -90,6 +80,161 @@ namespace OWLib.Types {
 
         public Vec3d(double x, double y, double z) {
             this.x = x; this.y = y; this.z = z;
+        }
+        public Vec3d(float ix, float iy, float iz) {
+            x = ix;
+            y = iy;
+            z = iz;
+        }
+        public Vec3d toDegrees() {
+            Vec3d r = new Vec3d();
+
+            // To degrees
+            r.x = (x / Math.PI) * 180.0;
+            r.y = (y / Math.PI) * 180.0;
+            r.z = (z / Math.PI) * 180.0;
+
+            return r;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct Vec4 {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+        public Vec4(float ix, float iy, float iz, float iw = 0.0f) {
+            x = ix;
+            y = iy;
+            z = iz;
+            w = iw;
+        }
+        public Vec4(double ix, double iy, double iz, double iw = 0.0) {
+            x = (float)ix;
+            y = (float)iy;
+            z = (float)iz;
+            w = (float)iw;
+        }
+        public Vec4(Vec3 iv) {
+            x = iv.x;
+            y = iv.y;
+            z = iv.z;
+            w = 1.0f;
+        }
+        public static implicit operator Vec4(Vec3 v) {
+            return new Vec4(v);
+        }
+    }
+
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct Vec4d {
+        public double x;
+        public double y;
+        public double z;
+        public double w;
+
+        public Vec4d(double x, double y, double z, double w = 0.0) {
+            this.x = x; this.y = y; this.z = z; this.w = w;
+        }
+        public Vec4d(float ix, float iy, float iz, float iw = 0.0f) {
+            x = ix;
+            y = iy;
+            z = iz;
+            w = iw;
+        }
+        public Vec4d(Vec3d iv) {
+            x = iv.x;
+            y = iv.y;
+            z = iv.z;
+            w = 0.0;
+        }
+        public static implicit operator Vec4d(Vec3d v) {
+            return new Vec4d(v);
+        }
+
+        public Vec4d fromEuler(double yaw, double pitch, double roll) {
+            double num = roll * 0.5f;
+            double num2 = Math.Sin((double)num);
+            double num3 = Math.Cos((double)num);
+            double num4 = pitch * 0.5f;
+            double num5 = Math.Sin((double)num4);
+            double num6 = Math.Cos((double)num4);
+            double num7 = yaw * 0.5f;
+            double num8 = Math.Sin((double)num7);
+            double num9 = Math.Cos((double)num7);
+            Vec4d result;
+            result.x = num9 * num5 * num3 + num8 * num6 * num2;
+            result.y = num8 * num6 * num3 - num9 * num5 * num2;
+            result.z = num9 * num6 * num2 - num8 * num5 * num3;
+            result.w = num9 * num6 * num3 + num8 * num5 * num2;
+            return result;
+        }
+        public Vec4d fromEuler(Vec3d input) {
+            return fromEuler(input.y, input.x, input.z);
+        }
+
+        public Vec3d toEuler() {
+            double[,] matrix = new double[4, 4];
+            {
+                double num = x * x + y * y + z * z + w * w;
+                double num2;
+                if (num > 0.0) {
+                    num2 = 2.0 / num;
+                } else {
+                    num2 = 0.0;
+                }
+                double num3 = x * num2;
+                double num4 = y * num2;
+                double num5 = z * num2;
+                double num6 = w * num3;
+                double num7 = w * num4;
+                double num8 = w * num5;
+                double num9 = x * num3;
+                double num10 = x * num4;
+                double num11 = x * num5;
+                double num12 = y * num4;
+                double num13 = y * num5;
+                double num14 = z * num5;
+                matrix[0, 0] = 1.0 - (num12 + num14);
+                matrix[0, 1] = num10 - num8;
+                matrix[0, 2] = num11 + num7;
+                matrix[1, 0] = num10 + num8;
+                matrix[1, 1] = 1.0 - (num9 + num14);
+                matrix[1, 2] = num13 - num6;
+                matrix[2, 0] = num11 - num7;
+                matrix[2, 1] = num13 + num6;
+                matrix[2, 2] = 1.0 - (num9 + num12);
+                matrix[3, 3] = 1.0;
+            }
+            {
+                int i = 0;
+                int j = 1;
+                int k = 2;
+                double[,] M = matrix;
+                Vec3d vec = new Vec3d();
+                double num2 = Math.Sqrt(M[i, i] * M[i, i] + M[j, i] * M[j, i]);
+                if (num2 > 0.00016) {
+                    vec.x = (float)Math.Atan2(M[k, j], M[k, k]);
+                    vec.y = (float)Math.Atan2(-M[k, i], num2);
+                    vec.z = (float)Math.Atan2(M[j, i], M[i, i]);
+                } else {
+                    vec.x = (float)Math.Atan2(-M[j, k], M[j, j]);
+                    vec.y = (float)Math.Atan2(-M[k, i], num2);
+                    vec.z = 0f;
+                }
+                return vec;
+            }
+        }
+        public static Vec4d operator +(Vec4d vector1, Vec4d vector2) {
+            Vec4d val;
+            val.x = vector1.x + vector2.x;
+            val.y = vector1.y + vector2.y;
+            val.z = vector1.z + vector2.z;
+            val.w = vector1.w + vector2.w;
+            return val;
         }
     }
 
