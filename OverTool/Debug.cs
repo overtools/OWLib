de6d6c29e7c5c30a311426006ec16963ad4e63ba
Naomi <naomichan@users.noreply.github.com>
2017-06-18 21:34:24 +0100

    Update Debug.cs

--- OverTool/Debug.cs
+++ OverTool/Debug.cs
@@ -11,27 +11,12 @@ namespace OverTool {
         public uint MinimumArgs => 0;
         public char Opt => '~';
         public string Title => "Debug";
-        public ushort[] Track => new ushort[1] { 0xA6 };
+        public ushort[] Track => new ushort[0];
         public bool Display => System.Diagnostics.Debugger.IsAttached;
 
         public void Parse(Dictionary<ushort, List<ulong>> track, Dictionary<ulong, Record> map, CASCHandler handler, bool quiet, OverToolFlags flags) {
             foreach (KeyValuePair<ushort, List<ulong>> pair in track) {
                 Console.Out.WriteLine($"{pair.Key:X3} {pair.Value.Count} entries");
-            }
-            // Code for rapidly finding what zero values are.
-            foreach (ulong key in track[0xA6]) {
-                using (Stream stream = Util.OpenFile(map[key], handler)) {
-                    STUD stud = new STUD(stream);
-                    if (stud.Instances == null) {
-                        continue;
-                    }
-
-                    if (stud.Instances[0] is TextureOverride @override) {
-                        if (@override.Header.unknown2 > 0 || @override.Header.unknown4 > 0 || @override.Header.unknown7 > 0 || @override.Header.unknown12.key > 0 || @override.Header.unknown13 > 0) {
-                            System.Diagnostics.Debugger.Break();
-                        }
-                    }
-                }
             }
         }
     }
