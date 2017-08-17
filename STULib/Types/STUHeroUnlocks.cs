ed2c57b1a4e42d1908153665f657c3a81a29c601
yukimono <yukimono6@gmail.com>
2017-08-17 21:39:47 +0100

    Old version compatability

--- STULib/Types/STUHeroUnlocks.cs
+++ STULib/Types/STUHeroUnlocks.cs
@@ -1,10 +1,21 @@
 ﻿using static STULib.Types.Generic.Common;
+#pragma warning disable 169
 
 namespace STULib.Types {
     [STU(0x33F56AC1)]
     public class STUHeroUnlocks : STUInstance {
+        [STUField(STUVersionOnly = new uint[] {1})]
+        private long padding0;
+
         [STUField(0x5C57AB57)]
         public UnlockInfo SystemUnlocks;
+
+        [STUField(STUVersionOnly = new uint[] {1})]
+        private long padding1;
+        [STUField(STUVersionOnly = new uint[] {1})]
+        private long padding2;
+        [STUField(STUVersionOnly = new uint[] {1})]
+        private long padding3;
 
         [STUField(0x719E981B)]
         public UnlockInfo[] Unlocks;
@@ -13,11 +24,17 @@ namespace STULib.Types {
         public EventUnlockInfo[] LootboxUnlocks;
 
         public class EventUnlockInfo {
-            [STUField(0x581570BA)]
-            public uint Event;
+            [STUField(STUVersionOnly = new uint[] {1})]
+            private long padding0;
 
             [STUField(0x719E981B, Padding = 8)]
             public UnlockInfo Data;
+
+            [STUField(0x581570BA)]
+            public uint Event;
+
+            [STUField(STUVersionOnly = new uint[] {1})]
+            private uint padding1;
         }
 
 
