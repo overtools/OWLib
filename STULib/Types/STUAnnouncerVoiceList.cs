37f9c24063ab38db760d5d7d1af84fd57b313a75
ZingBallyhoo <zingballyhoo@live.co.uk>
2017-08-18 19:55:34 +0100

    Add WIP STUAnnouncerVoiceList

---
+++ STULib/Types/STUAnnouncerVoiceList.cs
@@ -0,0 +1,14 @@
+﻿using static STULib.Types.Generic.Common;
+
+namespace STULib.Types {
+    [STU(0x248E334E)]
+    public class STUAnnouncerVoiceList : STUInstance {
+        [STUField(0xEAEF2657)]
+        public AnnouncerFXEntry[] Entries;
+
+        public class AnnouncerFXEntry {
+            [STUField(0x3152B773)]
+            public STUGUID Master;
+        }
+    }
+}
