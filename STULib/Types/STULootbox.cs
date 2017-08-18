b7692561a093f1802327088556d7e8d69dab9464
ZingBallyhoo <zingballyhoo@live.co.uk>
2017-08-18 18:19:58 +0100

    Add STULootbox

---
+++ STULib/Types/STULootbox.cs
@@ -0,0 +1,59 @@
+﻿using OWLib;
+using static STULib.Types.Generic.Common;
+
+namespace STULib.Types {
+    [STU(0x0A6886A1, "STULootbox")]
+    public class STULootbox : STUInstance {
+        [STUField(0x537B9B9E)]
+        public STUGUID Effect;
+
+        [STUField(0x6C7F44B7)]
+        public STUGUID Effect2;
+
+        [STUField(0x6D4304BF)]
+        public STUGUID Material;
+
+        [STUField(0x90A4CE09)]
+        public STUGUID StateScriptComponent;
+
+        [STUField(0xC08C4427)]
+        public STUGUID Name;
+
+        [STUField(0xC6C46D25)]
+        public LootboxBundle[] Bundldes;
+
+        [STUField(0xEA136681)]
+        public STUGUID Effect3;
+
+        [STUField(0xED9136D5)]
+        public uint Unknown1;  // 0
+
+        [STUField(0x581570BA)]
+        public uint EventID;
+
+        [STUField(0xC1458915)]
+        public STUGUID Unknown0C3;  // 0C3 isn't in CMF
+
+        [STUField(0xFCDFD33B)]
+        public STUGUID Effect4;
+
+        [STUField(0x40B14C7E)]
+        public STUGUID Material2;
+
+        [STUField(0x17A08ED7)]
+        public byte Unknown2;
+
+        [STUField(0xB1359AA2)]
+        public byte Unknown3;
+
+        public class LootboxBundle {
+            [STUField(0x294A8C84)]
+            public STUGUID String;
+            [STUField(0x6AA931B6)]
+            public STUGUID UnknownImage;
+        }
+
+        public string EventNameNormal => ItemEvents.GetInstance().GetEventNormal(EventID);
+        public string EventName => ItemEvents.GetInstance().GetEvent(EventID);
+    }
+}
