355f0f41ba5635535e1a53e076c5e40815485e60
Js41637 <Js41637@users.noreply.github.com>
2017-10-10 22:35:16 +1100

    Work on Extract Logic

---
+++ DataTool/SaveLogic/SprayAndImage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using static DataTool.Helper.IO;
+using DataTool.FindLogic;
+using DataTool.Flag;
+using DataTool.Models;
+using DataTool.ToolLogic.Extract;
+using static DataTool.Helper.STUHelper;
+using OWLib;
+using STULib.Types;
+
+namespace DataTool.SaveLogic {
+    public class SprayAndImage {
+        public static void SaveItems(string basePath, string heroName, string containerName, string folderName, ICLIFlags flags, IEnumerable<ulong> items) {
+            var ext = "dds";
+            if (flags is ExtractFlags extractFlags) {
+
+            }
+
+            foreach (var key in items) {
+                ItemInfo item = GatherUnlock(key);
+
+                if (item.Unlock.CosmeticTextureResource == null) continue;
+
+                var name = GetValidFilename(item.Name);
+                var texturePath = item.Unlock.CosmeticTextureResource;
+                var filePath = Path.Combine(basePath, containerName, folderName, $"{name}.{ext}");
+
+                STUDecal decal = GetInstance<STUDecal>(texturePath);
+
+                CreateDirectoryFromFile(filePath);
+
+                using (Stream fileStream = OpenFile(texturePath)) {
+                    if (fileStream == null) continue;
+                    using (Stream outputStream = File.OpenWrite(filePath)) {
+                        fileStream.CopyTo(outputStream);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ DataTool/ToolLogic/Extract/ExtractGeneral.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataTool.Flag;
+using DataTool.Models;
+using STULib.Types;
+using static DataTool.Program;
+using static DataTool.Helper.STUHelper;
+
+namespace DataTool.ToolLogic.Extract {
+    [Tool("extract-general", Description = "Extract general sprays and icons", TrackTypes = new ushort[] {0x54}, CustomFlags = typeof(ExtractFlags))]
+    public class ExtractGeneral : ITool {
+        public void IntegrateView(object sender) {
+            throw new NotImplementedException();
+        }
+
+        public void Parse(ICLIFlags toolFlags) {
+            GetGeneralUnlocks(toolFlags);
+        }
+
+        public void GetGeneralUnlocks(ICLIFlags toolFlags) {
+            string basePath;
+            if (toolFlags is ExtractFlags flags) {
+                basePath = flags.OutputPath;
+            } else {
+                throw new Exception("no output path");
+            }
+
+            foreach (var key in TrackedFiles[0x54]) {
+                STUGlobalInventoryMaster invMaster = GetInstance<STUGlobalInventoryMaster>(key);
+                if (invMaster == null) continue;
+
+                SaveLogic.SprayAndImage.SaveItems(basePath, null, "General", "Achievements", flags, invMaster.AchievementUnlocks?.Unlocks?.Select(it => (ulong)it));
+            }
+        }
+    }
+}
