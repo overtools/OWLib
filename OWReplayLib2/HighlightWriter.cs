b49010036169e9c28107e8c73e2d301ede77d551
ZingBallyhoo <zingballyhoo@live.co.uk>
2017-10-11 00:37:27 +0100

    Overwatch 1.16

---
+++ OWReplayLib2/HighlightWriter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using OWReplayLib.Types;
+using Highlight = OWReplayLib2.Types.Highlight;
+using static OWReplayLib2.Helper;
+
+namespace OWReplayLib2 {
+    public class HighlightWriterDataStore {
+        public OWReplayLib.Types.Highlight.HighlightFlags Flags;
+        public List<HighlightInfoDataStore> Info;
+        public List<Common.HeroInfo> HeroInfo;
+        // todo fillers
+
+        public class HighlightInfoDataStore {
+            public string Name;
+            public Highlight.HighlightInfo Actual;
+            
+            public HighlightInfoDataStore(Highlight.HighlightInfo info) {
+                Actual = info;
+            }
+        }
+    }
+    
+    public class HighlightWriter {
+        public HighlightReader Reader;
+        public Highlight.HighlightHeader Header;
+        protected HighlightWriterDataStore Data;
+        
+        public HighlightWriter(HighlightReader reader) {
+            Reader = reader;
+            Header = reader.Header;  // cloned
+            
+            Data = new HighlightWriterDataStore();
+
+            // Data.Info = Header.Info.ToList();
+        }
+
+        public MemoryStream Write() {
+            return null;
+        }
+
+        public void SetPlayerName(string name) {
+            foreach (HighlightWriterDataStore.HighlightInfoDataStore highlightInfoDataStore in Data.Info) {
+                highlightInfoDataStore.Name = name;
+            }
+        }
+    }
+}
