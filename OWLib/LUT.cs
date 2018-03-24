98acce881d8db5b4b41440bb8b5a6319d70fb4bb
Naomi <yukimono6@gmail.com>
2018-03-24 01:50:01 +0000

    [lut] sanity

--- OWLib/LUT.cs
+++ OWLib/LUT.cs
@@ -11,22 +11,23 @@ namespace OWLib
             {
                 "SPILUT 1.0",
                 "3 3",
-                "256 256 256",
+                "32 32 32",
             };
 
             List<string> lines = new List<string>();
+
+            float @base = (float)byte.MaxValue / 2.0f; // signed?
+            // float @base = (float)byte.MaxValue / 1.0f; // unsigned?
 
             for (int y = 0; y < 32; y++)
             {
                 for (int x = 0; x < 1024; x++)
                 {
-                    // int[] neutral = new int[] { (x % 64) * 4, (y % 64) * 4, (y / 2 + x / 16) }; // 512x512
-                    int[] neutral = new int[] { (x % 32) * 8, y * 8, (x / 32) * 8 }; // 1024x32
-                    // int[] neutral = new int[] { (x % 16) * 16, y * 16, (x / 16) * 16 }; // 256x16
+                    int[] neutral = new int[] { (x % 32), y, (x / 32) }; // 1024x32
 
                     string s = $"{neutral[0]} {neutral[1]} {neutral[2]} ";
 
-                    float[] rgb = new float[] { (float)lutimage.ReadByte() / (float)neutral[0], (float)lutimage.ReadByte() / (float)neutral[1], (float)lutimage.ReadByte() / (float)neutral[2] };
+                    float[] rgb = new float[] { (float)lutimage.ReadByte() / @base, (float)lutimage.ReadByte() / @base, (float)lutimage.ReadByte() / @base };
                     lutimage.ReadByte(); // alpha.
 
                     s += $"{rgb[0]} {rgb[1]} {rgb[2]}";
