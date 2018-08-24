766adc2e658593898556c0b2d284a719fe71195e
Naomi <yukimono6@gmail.com>
2018-08-24 20:40:20 +0100

    [ci] fix build

---
+++ DataTool/ToolLogic/Dbg/DebugMapDump.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using DataTool.Flag;
+using DataTool.ToolLogic.Extract;
+using TankLib;
+using TankLib.STU;
+using TankLib.STU.Types;
+using static DataTool.Helper.IO;
+using static DataTool.Helper.STUHelper;
+using static DataTool.Helper.Logger;
+
+namespace DataTool.ToolLogic.Debug
+{
+    [Tool("te-map-placable-dump", Description = "", TrackTypes = new ushort[] { 0x9F }, IsSensitive = true, CustomFlags = typeof(ExtractFlags))]
+    class DebugMapDump : ITool
+    {
+        public void IntegrateView(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Parse(ICLIFlags toolFlags)
+        {
+            var flags = toolFlags as ExtractFlags;
+            var testguids = flags.Positionals.Skip(3).Select(x => uint.Parse(x, System.Globalization.NumberStyles.HexNumber));
+            foreach (var guid in Program.TrackedFiles[0x9F])
+            {
+                if(testguids.Contains(teResourceGUID.Index(guid)))
+                {
+                    var path = Path.Combine(flags.OutputPath, "teMapPlacable", teResourceGUID.Index(guid).ToString("X"));
+                    STUMapHeader map = GetInstance<STUMapHeader>(guid);
+
+                    foreach(var t in Enum.GetValues(typeof(Enums.teMAP_PLACEABLE_TYPE)))
+                    {
+                        var teType = (Enums.teMAP_PLACEABLE_TYPE) t;
+                        if(teType == Enums.teMAP_PLACEABLE_TYPE.UNKNOWN)
+                        {
+                            continue;
+                        }
+                        var o = Path.Combine(path, teType.ToString());
+                        if (teMapPlaceableData.Manager.Types.ContainsKey(teType))
+                        {
+                            continue;
+                        }
+                        if(!Directory.Exists(o))
+                        {
+                            Directory.CreateDirectory(o);
+                        }
+                        teMapPlaceableData placable = GetPlaceableData(map, teType);
+                        for(int i = 0; i < placable.Header.PlaceableCount; ++i)
+                        {
+                            using (var f = File.OpenWrite(Path.Combine(o, placable.CommonStructures[i].UUID.Value.ToString("N"))))
+                            {
+                                f.Write(((teMapPlaceableDummy)placable.Placeables[i]).Data, 0, ((teMapPlaceableDummy)placable.Placeables[i]).Data.Length);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static teMapPlaceableData GetPlaceableData(STUMapHeader map, Enums.teMAP_PLACEABLE_TYPE modelGroup)
+        {
+            return GetPlaceableData(map, (byte)modelGroup);
+        }
+
+        public static teMapPlaceableData GetPlaceableData(STUMapHeader map, byte type)
+        {
+            using (Stream stream = OpenFile(map.GetChunkKey(type)))
+            {
+                if (stream == null) return null;
+                return new teMapPlaceableData(stream);
+            }
+        }
+    }
+}
---
+++ DataTool/ToolLogic/Dbg/DebugVoodoo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTool.Flag;
+using TankLib;
+using static DataTool.Helper.STUHelper;
+
+namespace DataTool.ToolLogic.Debug
+{
+    [Tool("brrap", Description = "I hear da call", TrackTypes = new ushort[] { 0x1B }, IsSensitive = true)]
+
+    class DebugVoodoo : ITool
+    {
+        public void IntegrateView(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Parse(ICLIFlags toolFlags)
+        {
+            var indices = new List<ulong>
+            {
+                0x0000000017C7,
+                0x0000000017C8,
+                0x0000000017F8,
+                0x0000000017FA,
+                0x00000000180B
+            };
+            foreach(var guid in Program.TrackedFiles[0x1B])
+            {
+                if(indices.Contains(teResourceGUID.LongKey(guid)))
+                {
+                    var stu = OpenSTUSafe(guid);
+                    System.Diagnostics.Debugger.Break();
+                }
+            }
+        }
+    }
+}
