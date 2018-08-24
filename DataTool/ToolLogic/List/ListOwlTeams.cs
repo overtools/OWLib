545f27e2e6656200141d4396f70f361e1217c466
Js41637 <Js41637@users.noreply.github.com>
2018-08-24 19:44:55 +1000

    [DataTool]: Slightly improve list owl teams + add json output

--- DataTool/ToolLogic/List/ListOwlTeams.cs
+++ DataTool/ToolLogic/List/ListOwlTeams.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using DataTool.DataModels;
 using DataTool.Flag;
 using static DataTool.Program;
@@ -6,18 +7,28 @@ using static DataTool.Helper.Logger;
 
 namespace DataTool.ToolLogic.List {
     [Tool("list-owl-teams", Description = "List OWL teams", TrackTypes = new ushort[] {0xEC}, CustomFlags = typeof(ListFlags), IsSensitive = true)]
-    public class ListOwlTeams : ITool {
+    public class ListOwlTeams : JSONTool, ITool {
         public void IntegrateView(object sender) {
             throw new NotImplementedException();
         }
 
-        public void Parse(ICLIFlags toolFlags) {
-            foreach (ulong key in TrackedFiles[0xEC]) {
-                TeamDefinition teamDef = new TeamDefinition(key);
+        public void Parse(ICLIFlags toolFlags) {;
+            var teams = TrackedFiles[0xEC].Select(key => new TeamDefinition(key));
+            
+            if (toolFlags is ListFlags flags)
+                if (flags.JSON) {
+                    ParseJSON(teams, flags);
+                    return;
+                }
+            
+            foreach (var team in teams) {
+                Log($"{team.FullName}");
+                Log($"\tDivision: {team.Division}");
                 
-                Log($"{teamDef.FullName} ({teamDef.Abbreviation})");
-                Log($"    Division: {teamDef.Division}");
+                if (!string.IsNullOrEmpty(team.Abbreviation))
+                    Log($"\tAbbreviation: {team.Abbreviation}");
                 
+                Log();
 
                 // todo: fix later or something
                 // tbh i don't think anyone is that interested
@@ -27,7 +38,6 @@ namespace DataTool.ToolLogic.List {
                 //Log($"\t\tColor 2: {teamColors.m_D74D5F6F.Color2?.Hex() ?? "N/A"}");
                 //Log($"\t\tColor 3: {teamColors.m_D74D5F6F.Color3?.Hex() ?? "N/A"}");
                 //Log($"\t\tColor 4: {teamColors.m_D74D5F6F.Color4?.Hex() ?? "N/A"}");
-                //Log();
             }
         }
     }
