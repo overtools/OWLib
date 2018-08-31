9fa9d7f927bfc54366cdec84879ef946c171c073
Naomi <yukimono6@gmail.com>
2018-09-01 00:41:57 +0100

    [datatool] fix crash

--- DataTool/Program.cs
+++ DataTool/Program.cs
@@ -182,13 +182,16 @@ namespace DataTool {
         }
 
         public static void ShutdownMisc() {
-            var dbPath = Flags.ScratchDBPath;
-            if (!File.Exists(dbPath) || new DirectoryInfo(dbPath).Exists) {
-                dbPath = Path.Combine(Path.GetFullPath(Flags.ScratchDBPath), "Scratch.db");
-            }
-            if(Flags.Deduplicate && !string.IsNullOrWhiteSpace(dbPath)) {
-                TankLib.Helpers.Logger.Warn("ScratchDB", "Saving deduplication database...");
-                SaveLogic.Combo.ScratchDBInstance.Save(dbPath);
+            if (!string.IsNullOrWhiteSpace(Flags.ScratchDBPath)) {
+                var dbPath = Flags.ScratchDBPath;
+                if (!File.Exists(dbPath) || new DirectoryInfo(dbPath).Exists) {
+                    dbPath = Path.Combine(Path.GetFullPath(Flags.ScratchDBPath), "Scratch.db");
+                }
+
+                if (Flags.Deduplicate && !string.IsNullOrWhiteSpace(dbPath)) {
+                    TankLib.Helpers.Logger.Warn("ScratchDB", "Saving deduplication database...");
+                    SaveLogic.Combo.ScratchDBInstance.Save(dbPath);
+                }
             }
         }
 
