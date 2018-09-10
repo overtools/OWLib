e8d4d4fd099f7fc6f63770b78d4c75d9f8a11442
zingballyhoo <zingballyhoo@live.co.uk>
2018-09-10 20:29:27 +0100

    [tanklib]: fix RayCast STU names

--- TankLib/teShaderInstance.cs
+++ TankLib/teShaderInstance.cs
@@ -66,17 +66,12 @@ namespace TankLib {
             /// <summary>Type of shader resource this is</summary>
             public ShaderResourceType Type;  // 6
             public byte Format;  // 7
-            public byte UnknownD;
+            public ViewDimension ViewDimension;
             public short Zero;
             
             /// <summary>Global resource index</summary>
             /// <note>This is calculated at runtime, CRC is looked up in an array in the client</note>
             public short GlobalIndex;
-        }
-
-        public enum ShaderResourceType : byte {
-            Buffer = 0,
-            Texture = 1
         }
         
         [StructLayout(LayoutKind.Sequential, Pack = 4)]
@@ -90,6 +85,67 @@ namespace TankLib {
             /// <summary>Global sampler index</summary>
             /// <note>This is calculated at runtime, CRC is looked up in an array in the client</note>
             public short GlobalIndex;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 4)]
+        public struct InputElementDefinition {
+            public ShaderInputUse InputUse;
+            public byte Register;
+            public ShaderInputForm Format;
+
+            public byte Slot;
+            public byte Unk1;
+            public byte Unk2;
+            public byte Unk3;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct BufferPart {
+            /// <summary>"Hash" of the data. Not sure if it is actually a hash or not</summary>
+            public uint Hash;
+            
+            /// <summary>Size in bytes</summary>
+            public ushort Size;
+            public ushort Unk2Offset;
+            public ushort Unk3Offset;
+            
+            /// <summary>Offset in bytes</summary>
+            public ushort Offset;
+            
+            // unsure:
+            public byte ElementSize;
+            public byte ElementCount;
+            
+            /// <summary>If the "part" is global value or not?</summary>
+            public byte IsDynamic; // todo: bool
+            public byte Unknown;
+        }
+        
+        [StructLayout(LayoutKind.Sequential, Pack = 4)]
+        public struct BufferHeader {
+            /// <summary>Offset to "part" definitions</summary>
+            public long PartOffset;
+            
+            /// <summary>Offset to skeleton byte array</summary>
+            public long SkeletonOffset;
+            
+            /// <summary>Identifier hash</summary>
+            public ulong Hash;
+            
+            /// <summary>Size in bytes</summary>
+            public int BufferSize;
+            
+            /// <summary>Number of "parts" that make up this buffer</summary>
+            /// <note>Most buffers now have no "parts" definied in the 086 data because blizzard keeps reading my code</note>
+            public short PartCount;
+            
+            /// <summary>DX11 register</summary>
+            public short Register;
+        }
+
+        public enum ShaderResourceType : byte {
+            Buffer = 0,
+            Texture = 1
         }
 
         [SuppressMessage("ReSharper", "InconsistentNaming")]
@@ -124,16 +180,22 @@ namespace TankLib {
             UInt2 = 17
         }
 
-        [StructLayout(LayoutKind.Sequential, Pack = 4)]
-        public struct InputElementDefinition {
-            public ShaderInputUse InputUse;
-            public byte Register;
-            public ShaderInputForm Format;
-
-            public byte Slot;
-            public byte Unk1;
-            public byte Unk2;
-            public byte Unk3;
+        public enum ViewDimension : byte {
+            Buffer = 0, // ?
+            
+            SRVTexture1D = 1,
+            SRVTexture2D = 2,
+            SRVTexture3D = 3,
+            SRVTextureCube = 4,
+            SRVTexture1DArray = 5,
+            SRVTexture2DArray = 6,
+            SRVTextureCubeArray = 7,
+            
+            UAVTexture1D = 8,
+            UAVTexture2D = 9,
+            UAVTexture3D = 10,
+            UAVTexture1DArray = 11,
+            UAVTexture2DArray = 12
         }
 
         /// <summary>Header data</summary>
@@ -230,50 +292,6 @@ namespace TankLib {
                     reader.BaseStream.Position = end;
                 }
             }
-        }
-
-        [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        public struct BufferPart {
-            /// <summary>"Hash" of the data. Not sure if it is actually a hash or not</summary>
-            public uint Hash;
-            
-            /// <summary>Size in bytes</summary>
-            public ushort Size;
-            public ushort Unk2Offset;
-            public ushort Unk3Offset;
-            
-            /// <summary>Offset in bytes</summary>
-            public ushort Offset;
-            
-            // unsure:
-            public byte ElementSize;
-            public byte ElementCount;
-            
-            /// <summary>If the "part" is global value or not?</summary>
-            public byte IsDynamic; // todo: bool
-            public byte Unknown;
-        }
-        
-        [StructLayout(LayoutKind.Sequential, Pack = 4)]
-        public struct BufferHeader {
-            /// <summary>Offset to "part" definitions</summary>
-            public long PartOffset;
-            
-            /// <summary>Offset to skeleton byte array</summary>
-            public long SkeletonOffset;
-            
-            /// <summary>Identifier hash</summary>
-            public ulong Hash;
-            
-            /// <summary>Size in bytes</summary>
-            public int BufferSize;
-            
-            /// <summary>Number of "parts" that make up this buffer</summary>
-            /// <note>Most buffers now have no "parts" definied in the 086 data because blizzard keeps reading my code</note>
-            public short PartCount;
-            
-            /// <summary>DX11 register</summary>
-            public short Register;
         }
     }
 }
