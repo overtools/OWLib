a05c74576c0db56a72fa6ecb8ebb0e0b16acb87d
Js41637 <js41637@gmail.com>
2019-08-15 22:02:44 +1000

    [tankview] add progress bar when playing sounds

--- TankView/View/PreviewDataSound.xaml.cs
+++ TankView/View/PreviewDataSound.xaml.cs
@@ -4,17 +4,37 @@ using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
+using DataTool.WPF;
+using TankView.ViewModel;
 
 namespace TankView.View {
-    public partial class PreviewDataSound : UserControl, IDisposable {
+    public partial class PreviewDataSound : UserControl, INotifyPropertyChanged, IDisposable {
+        public SynchronizationContext ViewContext { get; }
         private WaveOutEvent outputDevice;
         private VorbisWaveReader vorbis;
+        public ProgressInfo ProgressInfo { get; set; }
+        private ProgressWorker _progressWorker = new ProgressWorker();
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string name) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
 
         public PreviewDataSound() {
             InitializeComponent();
+            ViewContext = SynchronizationContext.Current;
+            ProgressInfo = new ProgressInfo();
+            
+            _progressWorker.OnProgress += UpdateProgress;
+            
+            var timer = new System.Timers.Timer();
+            timer.Interval = 300;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
         }
 
         public void Dispose() {
@@ -28,6 +48,7 @@ namespace TankView.View {
                 vorbis = new VorbisWaveReader(data);
                 outputDevice.Volume = 1.0f;
                 outputDevice.Init(vorbis);
+                _progressWorker.ReportProgress(0, $"00:00/{new DateTime(vorbis.TotalTime.Ticks):mm:ss}");
             } catch (Exception ex) {
                 Debugger.Log(0, "[TankView.Sound.SetAudio]", $"Error setting audio! {ex.Message}\n");
                 // ignored
@@ -79,6 +100,32 @@ namespace TankView.View {
             }
 
             outputDevice.Pause();
+        }
+        
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            UpdateProgressBar();
+        }
+        
+        private void UpdateProgressBar()
+        {
+            if (outputDevice.PlaybackState == PlaybackState.Playing) {
+                var progress = (int) Math.Round(((float) vorbis.CurrentTime.Ticks / (float) vorbis.TotalTime.Ticks) * 1000);
+                _progressWorker.ReportProgress(progress, $"{new DateTime(vorbis.CurrentTime.Ticks):mm:ss}/{new DateTime(vorbis.TotalTime.Ticks):mm:ss}");
+            }
+        }
+        
+        private void UpdateProgress(object sender, ProgressChangedEventArgs @event) {
+            ViewContext.Send(x => {
+                if (!(x is ProgressChangedEventArgs evt)) return;
+                if (evt.UserState != null && evt.UserState is string state) {
+                    ProgressInfo.State = state;
+                }
+
+                ProgressInfo.Percentage = evt.ProgressPercentage;
+            }, @event);
+
+            NotifyPropertyChanged(nameof(ProgressInfo));
         }
     }
 }
