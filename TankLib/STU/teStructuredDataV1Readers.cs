e1ee4fcea431183ae73f2d6dfa0933b19cf7facd
zingballyhoo <zingballyhoo@live.co.uk>
2019-08-24 16:53:23 +0100

    [cleanup] remove unused files

--- TankLib/STU/teStructuredDataV1Readers.cs
+++
@@ -1,5 +0,0 @@
-namespace TankLib.STU {
-    public static class teStructuredDataV1Readers {
-        
-    }
-}
--- TankLib/teAssetPackagePayload.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace TankLib {
-    public class teAssetPackagePayload {
-        
-    }
-}
