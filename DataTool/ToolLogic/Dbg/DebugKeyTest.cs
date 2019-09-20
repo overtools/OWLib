153ce66361dbab8a610ab9a2c27bbed0393d9593
Naomi <614231+healingbrew@users.noreply.github.com>
2019-09-21 00:27:23 +0100

    Brickstion key

---
+++ DataTool/ToolLogic/Dbg/DebugKeyTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTool.Flag;
+using TACTLib.Core;
+using TACTLib.Core.Product.Tank;
+
+namespace DataTool.ToolLogic.Dbg
+{
+    [Tool("te-key-test", Description = "", IsSensitive = true, CustomFlags = typeof(ToolFlags))]
+    class DebugKeyTest : ITool
+    {
+        public void Parse(ICLIFlags toolFlags)
+        {
+            foreach(var guid in Program.TankHandler.Assets.Keys)
+            {
+                var stream = Program.TankHandler.OpenFile(guid);
+                var blte = stream as BLTEStream ?? (stream as GuidStream)?.BaseStream as BLTEStream;
+                if (blte != null)
+                {
+                    if(blte.Keys.Contains("6FDE3253A9819DD8")) {
+
+                    }
+                }
+            }
+        }
+    }
+}
