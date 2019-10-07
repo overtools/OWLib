3019fdcea9fb708e6259bd20b854e8770d425a08
Naomi Ahmed <naomi@pub.chomp.gg>
2019-10-07 19:40:29 +0100

    [fluff] add method for logging progress bars.

--- TankLib/Helpers/Logger.cs
+++ TankLib/Helpers/Logger.cs
@@ -185,5 +185,45 @@ namespace TankLib.Helpers {
             writer.WriteLine();
             return builder.ToString();
         }
+        
+        public static void LogProgress(string message, string pre, string post, double value, XTermColor messageColor, XTermColor preColor, XTermColor postColor, XTermColor brickColor, XTermColor processColor, bool showProgressValue, XTermColor processValueColor) {
+            if (Console.IsOutputRedirected) return;
+            var width = Console.WindowWidth;
+            var empty = new char[width];
+            Array.Fill(empty, ' ');
+            if (message != LastMessage) {
+                Console.Out.Write(empty);
+                Console.CursorLeft = 0;
+                LastMessage = message;
+                Logger.Log24Bit(messageColor, true, Console.Out, null, message);
+            }
+            Console.Out.Write(empty);
+            Console.CursorLeft = 0;
+            var remaining = width - pre.Length - post.Length - 4;
+            Logger.Log24Bit(preColor, false, Console.Out, null, pre);
+            if (remaining > 0) {
+                Logger.Log24Bit(brickColor, false, Console.Out, null, " [");
+                empty = new char[remaining];
+                Array.Fill(empty, ' ');
+                Array.Fill(empty, '=', 0, (int)Math.Round(remaining * Math.Min(value, 1)));
+                Logger.Log24Bit(processColor, false, Console.Out, null, string.Join("", empty));
+                Logger.Log24Bit(brickColor, false, Console.Out, null, "] ");
+
+                if(showProgressValue && remaining > 6) {
+                    var valueText = (Math.Min(value, 1) * 100).ToString().Split('.')[0] + "%";
+                    Console.CursorLeft = pre.Length + 2 + (int)Math.Floor(remaining / 2.0d - valueText.Length / 2.0d);
+                    Logger.Log24Bit(processValueColor, false, Console.Out, null, valueText);
+                    Console.CursorLeft = width - post.Length;
+                }
+            }
+            Logger.Log24Bit(postColor, false, Console.Out, null, post);
+            Console.CursorLeft = Console.WindowWidth - 1;
+            Console.CursorTop -= 1;
+        }
+        
+        public static void FlushProgress() {
+            Console.CursorTop += 1;
+            Console.WriteLine();
+        }
     }
 }
