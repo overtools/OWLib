18b13587cb48460eec47757792c13c4ed16c4e3d
Js41637 <js41637@gmail.com>
2020-03-20 18:44:13 +1100

    Update ExtractHeroVoiceBetter.cs

--- DataTool/ToolLogic/Extract/ExtractHeroVoiceBetter.cs
+++ DataTool/ToolLogic/Extract/ExtractHeroVoiceBetter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using DataTool.DataModels;
@@ -13,7 +14,16 @@ using static DataTool.Helper.STUHelper;
 using static DataTool.Helper.IO;
 
 namespace DataTool.ToolLogic.Dbg {
-    [Tool("extract-hero-voice-better", Description = "Extracts hero voicelines but groups them a bit better.", CustomFlags = typeof(ExtractFlags))]
+    public class BetterVoiceFlags : ExtractFlags
+    {
+        [CLIFlag(Default = false, Flag = "group-by-hero", Hidden = true, Parser = new[] { "DataTool.Flag.Converter", "CLIFlagBoolean" })]
+        public bool GroupByHero;
+        
+        [CLIFlag(Default = true, Flag = "group-by-type", Hidden = true, Parser = new[] { "DataTool.Flag.Converter", "CLIFlagBoolean" })]
+        public bool GroupByType;
+    }
+    
+    [Tool("extract-hero-voice-better", Description = "Extracts hero voicelines but groups them a bit better.", CustomFlags = typeof(BetterVoiceFlags))]
     class ExtractHeroVoiceBetter : JSONTool, ITool {
         public class VoiceGroup {
             public string Name;
@@ -55,7 +65,8 @@ namespace DataTool.ToolLogic.Dbg {
             new VoiceGroup { Name = "TeamElimination", StimulusSetIds = new []{ "00000000001B.078" } },
             new VoiceGroup { Name = "Revenge",         StimulusSetIds = new []{ "000000000036.078" } },
             new VoiceGroup { Name = "Fallback",        StimulusSetIds = new []{ "000000000111.078" } },
-            new VoiceGroup { Name = "EnemyTeleporter", StimulusSetIds = new []{ "0000000002F0.078" } },
+            new VoiceGroup { Name = "EnemyTeleporter", StimulusSetIds = new []{ "0000000002F0.078", "0000000001DF.078", "0000000001E0.078", "0000000001E1.078" } },
+            new VoiceGroup { Name = "EnemyTurret",     StimulusSetIds = new []{ "000000000037.078", "0000000000D8.078" } },
             new VoiceGroup { Name = "GoingIn",         StimulusSetIds = new []{ "0000000002E8.078" } },
             new VoiceGroup { Name = "ImWithYou",       StimulusSetIds = new []{ "00000000010F.078" } },
             new VoiceGroup { Name = "OnMyWay",         StimulusSetIds = new []{ "0000000002E9.078" } },
@@ -64,13 +75,43 @@ namespace DataTool.ToolLogic.Dbg {
             new VoiceGroup { Name = "GetReady",        StimulusSetIds = new []{ "000000000573.078" } },
             new VoiceGroup { Name = "GetInThere",      StimulusSetIds = new []{ "0000000002C5.078" } },
             new VoiceGroup { Name = "RescueTeammate",  StimulusSetIds = new []{ "0000000000CA.078" } },
+            new VoiceGroup { Name = "Karaoke",         StimulusSetIds = new []{ "000000000A27.078" } },
+            new VoiceGroup { Name = "Armor",           StimulusSetIds = new []{ "0000000002F8.078" } },
             new VoiceGroup { Name = "TeamKill",        StimulusSetIds = new []{ "000000000584.078", "000000000584.078" } }, // why is there 2
             new VoiceGroup { Name = "UnderAttack",     StimulusSetIds = new []{ "0000000009C6.078" } }, // supports only
+            new VoiceGroup { Name = "Jumping",         StimulusSetIds = new []{ "000000000054.078" } },
+            new VoiceGroup { Name = "DeathCharged",    StimulusSetIds = new []{ "000000000022.078" } },
+            new VoiceGroup { Name = "DeathExplosion",  StimulusSetIds = new []{ "00000000001E.078" } },
+            new VoiceGroup { Name = "Stagger",         StimulusSetIds = new []{ "0000000000B7.078" } },
+            new VoiceGroup { Name = "Cough",           StimulusSetIds = new []{ "0000000000CD.078" } },
+            new VoiceGroup { Name = "Payload",         StimulusSetIds = new []{ "000000000027.078" } },
+            new VoiceGroup { Name = "Damaged",         StimulusSetIds = new []{ "000000000283.078" } },
+            new VoiceGroup { Name = "Electrocuted",    StimulusSetIds = new []{ "00000000012F.078" } },
+            new VoiceGroup { Name = "Scream",          StimulusSetIds = new []{ "000000000113.078" } },
+            new VoiceGroup { Name = "Death",           StimulusSetIds = new []{ "000000000001.078" } },
+            new VoiceGroup { Name = "MatchTalk",       StimulusSetIds = new []{ "00000000000D.078" } },
+            new VoiceGroup { Name = "LevelUp",         StimulusSetIds = new []{ "00000000026F.078" } },
+            new VoiceGroup { Name = "Laugh",           StimulusSetIds = new []{ "0000000006A1.078" } },
+            new VoiceGroup { Name = "Junkenstein",     StimulusSetIds = new []{ "0000000006BE.078", "0000000006AB.078", "000000000854.078", "000000000852.078" } },
+            new VoiceGroup { Name = "StormRising",     StimulusSetIds = new []{ "000000000D1C.078", "000000000D1F.078", "000000000D16.078", "000000000D14.078", "000000000D15.078" } },
+            new VoiceGroup { Name = "Retribution",     StimulusSetIds = new []{ "0000000009CC.078", "000000000A10.078", "000000000968.078" } },
+            new VoiceGroup { Name = "Uprising",        StimulusSetIds = new []{ "000000000778.078", "0000000007F5.078", "00000000078E.078", "00000000078D.078", "000000000789.078", "000000000778.078" } },
+            
+            // These heroes don't have traditional ultimate voicelines like other heroes
+            new VoiceGroup { Name = "Ultimate", StimulusSetIds = new []
+            {
+                "000000000093.078", // reinhardt
+                "000000000085.078", // widowmaker
+                "000000000082.078", // tracer
+                "000000000081.078", // tracer
+                "00000000048B.078", // torbjorn
+                "00000000014A.078" // torbjorn
+            } },
         };
 
         public void Parse(ICLIFlags toolFlags) {
             string basePath;
-            if (toolFlags is ExtractFlags flags) {
+            if (toolFlags is BetterVoiceFlags flags) {
                 basePath = flags.OutputPath;
             } else {
                 throw new Exception("no output path");
@@ -98,15 +139,24 @@ namespace DataTool.ToolLogic.Dbg {
 
                             if (stimulus == null) continue;
                             
-                            var groupName = GetVoiceGroup(voiceLineInstance.VoiceStimulus, stimulus.m_category, stimulus.m_87DCD58E);
-                            if (groupName == null)
-                                continue;
+                            var groupName = GetVoiceGroup(voiceLineInstance.VoiceStimulus, stimulus.m_category, stimulus.m_87DCD58E) ?? "Unknown";
                             
                             foreach (var soundFile in voiceLineInstance.SoundFiles) {
                                 Combo.Find(soundFilesCombo, soundFile);
                             }
-                            
-                            SaveLogic.Combo.SaveAllVoiceSoundFiles(toolFlags, Path.Combine(basePath, Container, groupName, heroNameActual), soundFilesCombo);                            
+
+                            var path = flags.GroupByHero && flags.GroupByType
+                                ? Path.Combine(basePath, Container, heroNameActual, groupName)
+                                : Path.Combine(basePath, Container, flags.GroupByHero ? Path.Combine(groupName, heroNameActual) : groupName);
+
+                            foreach (var soundInfo in soundFilesCombo.VoiceSoundFiles.Values) {
+                                var filename = soundInfo.GetName();
+                                if (!flags.GroupByHero) {
+                                    filename = $"{heroNameActual}-{soundInfo.GetName()}";
+                                }
+                                
+                                SaveLogic.Combo.SaveSoundFile(flags, path, soundFilesCombo, soundInfo.GUID, true, filename);
+                            }                    
                         }
                     }
                 }
