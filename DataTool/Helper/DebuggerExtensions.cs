95a2f032089a9ed6c1d81474ab474f12b14d4966
Js41637 <js41637@gmail.com>
2020-08-04 13:38:31 +1000

    add debugger extension

---
+++ DataTool/Helper/DebuggerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using TankLib;
+using TankLib.STU;
+
+namespace DataTool.Helper {
+    public static class DebuggerExtensions {
+        public static string GetString(this teStructuredDataAssetRef<ulong> stu) {
+            if (teResourceGUID.Type(stu) == 0x7C) {
+                return IO.GetString(stu);
+            }
+
+            return null;
+        }
+    }
+}
