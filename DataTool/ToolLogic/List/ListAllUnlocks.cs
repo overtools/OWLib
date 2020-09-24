63ed582b17db06893c2f2fdeab98441f157e1f85
Js41637 <js41637@gmail.com>
2020-09-24 19:02:48 +1000

    [datatool] add the hero the list all unlocks command

--- DataTool/ToolLogic/List/ListAllUnlocks.cs
+++ DataTool/ToolLogic/List/ListAllUnlocks.cs
@@ -1,18 +1,51 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using DataTool.DataModels;
+using DataTool.DataModels.Hero;
 using DataTool.Flag;
+using DataTool.Helper;
 using DataTool.JSON;
+using TankLib;
+using TankLib.STU.Types;
 using static DataTool.Program;
 
 namespace DataTool.ToolLogic.List {
-    [Tool("list-all-unlocks", Description = "List hero unlocks", CustomFlags = typeof(ListFlags), IsSensitive = true)]
+    [Tool("list-all-unlocks", Description = "List all unlocks", CustomFlags = typeof(ListFlags), IsSensitive = true)]
     public class ListAllUnlocks : JSONTool, ITool {
         public void Parse(ICLIFlags toolFlags) {
-            var data = TrackedFiles[0xA5].Select(key => new Unlock(key)).Where(unlock => unlock.GUID != 0).ToList();
+            var allUnlocks = new Dictionary<teResourceGUID, UnlockAll>();
+            
+            foreach (var key in TrackedFiles[0xA5]) {
+                var guid = (teResourceGUID) key;
+                if (!allUnlocks.ContainsKey(guid)) {
+                    var unlock = new UnlockAll(key);
+                    if (unlock.GUID != 0) {
+                        allUnlocks[guid] = unlock;
+                    }
+                }
+            }
+
+            var heroes = TrackedFiles[0x75].Select(STUHelper.GetInstance<STUHero>);
+            
+            foreach (var stuHero in heroes) {
+                var hero = new Hero(stuHero);
+                var progression = new ProgressionUnlocks(stuHero);
+                foreach (var unlock in progression.IterateUnlocks()) {
+                    if (allUnlocks.ContainsKey(unlock.GUID)) {
+                        allUnlocks[unlock.GUID].Hero = hero.Name;
+                    }
+                }
+            }
 
             if (toolFlags is ListFlags flags) {
-                OutputJSON(data, flags);
+                OutputJSON(allUnlocks, flags);
             }
         }
+    }
+    
+    public class UnlockAll : Unlock {
+        public string Hero;
+
+        public UnlockAll(ulong guid) : base(guid) { }
     }
 }
