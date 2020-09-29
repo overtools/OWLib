2b2f57f7745d7581e53d8ee5bbd32806a19807ea
Js41637 <js41637@gmail.com>
2020-09-29 18:58:17 +1000

    [datatool] list game tips

---
+++ DataTool/ToolLogic/List/ListTips.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using DataTool.Flag;
+using DataTool.Helper;
+using DataTool.JSON;
+using TankLib;
+using TankLib.STU.Types;
+using static DataTool.Program;
+using static DataTool.Helper.Logger;
+
+namespace DataTool.ToolLogic.List {
+    [Tool("list-tips", Description = "List game tips", CustomFlags = typeof(ListFlags))]
+    public class ListTips : JSONTool, ITool {
+        public void Parse(ICLIFlags toolFlags) {
+            var data = GetData();
+
+            if (toolFlags is ListFlags flags)
+                if (flags.JSON) {
+                    OutputJSON(data, flags);
+                    return;
+                }
+
+            var i = new IndentHelper();
+            foreach (var item in data) {
+                Log($"{item.Key}");
+                Log($"{i+1}{item.Value}");
+            }
+        }
+
+        private Dictionary<teResourceGUID, string> GetData() {
+            var @return = new Dictionary<teResourceGUID, string>();
+
+            foreach (teResourceGUID key in TrackedFiles[0xD5]) {
+                var stu = STUHelper.GetInstance<STU_96ABC153>(key);
+                if (stu == null) continue;
+
+                @return[key] = IO.GetString(stu.m_94672A2A);
+            }
+
+            return @return;
+        }
+    }
+}
