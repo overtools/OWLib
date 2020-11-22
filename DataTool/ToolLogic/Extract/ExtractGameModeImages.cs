4833b6c963dcfa530965e638e5caa976672a236d
Js41637 <js41637@gmail.com>
2020-11-23 09:46:48 +1100

    [datatool] add proper command to extract gamemode images

---
+++ DataTool/ToolLogic/Extract/ExtractGameModeImages.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using DataTool.FindLogic;
+using DataTool.Flag;
+using TankLib;
+using TankLib.STU;
+using TankLib.STU.Types;
+using static DataTool.Program;
+using static DataTool.Helper.IO;
+
+namespace DataTool.ToolLogic.Extract {
+    [Tool("extract-gamemode-images", Description = "Extracts gamemode and arcade images", CustomFlags = typeof(ExtractFlags))]
+    public class ExtractGamemodeImages : ITool {
+        public void Parse(ICLIFlags toolFlags) {
+            string basePath;
+            if (toolFlags is ExtractFlags flags) {
+                basePath = flags.OutputPath;
+            } else {
+                throw new Exception("no output path");
+            }
+
+            const string container = "GamemodeImages";
+            string path = Path.Combine(basePath, container);
+            
+            foreach (ulong key in TrackedFiles[0xEE]) {
+                var stuE3594B8E = Helper.STUHelper.GetInstance<STU_E3594B8E>(key);
+
+                if (stuE3594B8E == null) {
+                    continue;
+                }
+
+                string name = $"{teResourceGUID.Index(key):X3}_{GetString(stuE3594B8E.m_name)}";
+                
+                Combo.ComboInfo info = new Combo.ComboInfo();
+                Combo.Find(info, (ulong)stuE3594B8E.m_21EB3E73);
+                info.SetTextureName((ulong)stuE3594B8E.m_21EB3E73, name);
+
+                var context = new SaveLogic.Combo.SaveContext(info);
+                SaveLogic.Combo.SaveLooseTextures(flags, path, context);
+                context.Wait();
+            }
+        }
+    }
+}
