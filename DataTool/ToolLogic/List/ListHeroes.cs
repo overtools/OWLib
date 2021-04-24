1ec14600a4aa2cd75a98a06c8f117cd32d543ae3
Prophet <29117146+prophetl33t@users.noreply.github.com>
2021-04-24 22:20:54 +0700

    more volley data and stuvar things

--- DataTool/ToolLogic/List/ListHeroes.cs
+++ DataTool/ToolLogic/List/ListHeroes.cs
@@ -42,13 +42,15 @@ namespace DataTool.ToolLogic.List {
 
                     TankLib.Helpers.Logger.Log24Bit(ConsoleColor.White.AsDOSColor().AsXTermColor().ToForeground(), null, false, Console.Out, null, $"{indentLevel + 1}sRGB Color: {hero.Value.GalleryColor.ToNonLinear().ToHex()} ");
                     TankLib.Helpers.Logger.Log24Bit(hero.Value.GalleryColor.ToNonLinear().ToForeground(), null, true, Console.Out, null, "██████");
+                    bool shown = false;
                     if (hero.Value.Loadouts != null) {
                         Log($"{indentLevel + 1}Loadouts:");
                         foreach (var loadout in hero.Value.Loadouts) {
                             Log($"{indentLevel + 2}{loadout.Name}: {loadout.Category}");
-                            if (loadout.Category == TankLib.STU.Types.Enums.LoadoutCategory.Weapon) {
+                            if (loadout.Category == TankLib.STU.Types.Enums.LoadoutCategory.Weapon && !shown) {
                                 // Weapon stats dump.
                                 // 000000001683.01B is Torbjorn gun with 18 ammo
+                                shown = true;
                                 foreach (var weapon in GetInstance<STUWeaponComponent>(hero.Value.STU.m_gameplayEntity.GUID).m_weapons) {
                                     if (weapon.m_script != null) {
                                         List<STUConfigVarExpression> weap_vars = new List<STUConfigVarExpression>(GetInstances<STUConfigVarExpression>(weapon.m_script.GUID));
@@ -56,23 +58,20 @@ namespace DataTool.ToolLogic.List {
                                         //melee stats (TODO)
                                         var spec_containters = GetInstances<STUStatescriptActionPlayScript>(weapon.m_script.GUID);
                                         if (spec_containters.Length != 0) {
-                                            foreach (var script_ref in spec_containters)
-                                            weap_vars.AddRange(GetInstances<STUConfigVarExpression>((script_ref.m_script as STU_105E1BCC)?.m_graph));
+                                            foreach (var script_ref in spec_containters) {
+                                                var insts = GetInstances<STUConfigVarExpression>((script_ref.m_script as STU_105E1BCC).m_graph);
+                                                weap_vars.AddRange(insts);
+                                            }
                                         }
 
-                                        //projectile params
+                                        //volley params
                                         var projectile_params = GetInstance<STUStatescriptStateWeaponVolley>(weapon.m_script.GUID);
                                         if (projectile_params != null) {
-                                            var pps = (projectile_params.m_numProjectilesPerShot as STUConfigVarInt)?.m_value; 
-                                            if (pps != null && pps != 999)
-                                            Log($"{indentLevel + 2}Number of projectiles per shot: {pps}");
-
-                                            var sps = (projectile_params.m_numShotsPerSecond as STUConfigVarInt)?.m_value;
-                                            if (sps != null && sps != 999)
-                                            Log($"{indentLevel + 2}Number of shots per second: {sps}");
-
-                                            if (projectile_params.m_projectileMotions != null)
-                                            Log($"{indentLevel + 2}Speed of projectile (m/s): {((projectile_params.m_projectileMotions[0] as STU_E1A5B579)?.m_599AB8D7 as STUConfigVarFloat)?.m_value}");
+                                            PrintSTUVar($"{indentLevel + 2}Number of projectiles per shot", projectile_params.m_numProjectilesPerShot, weapon.m_script.GUID);
+                                            PrintSTUVar($"{indentLevel + 2}Number of shots per second", projectile_params.m_numShotsPerSecond, weapon.m_script.GUID);
+                                            PrintSTUVar($"{indentLevel + 2}Speed of projectile (m/s)", projectile_params.m_projectileSpeed, weapon.m_script.GUID);
+                                            PrintSTUVar($"{indentLevel + 2}Damage of projectile (direct hit)", projectile_params?.m_D2D11CE9?.m_modifyHealth.m_amount, weapon.m_script.GUID);
+                                            //PrintSTUVar($"{indentLevel + 2}Projectile knockback radius (max)", projectile_params?.m_D2D11CE9?.m_modifyHealth.m_knockbackRings[0].m_radius, weapon.m_script.GUID);
                                         }
 
                                         //generic stats
@@ -102,6 +101,49 @@ namespace DataTool.ToolLogic.List {
                     //{ new byte[] { 8, 0, 6, 0,25,0 } ,"Weapon shots per second "},
                     { new byte[] { 6, 0, 14, 0,25,14,1,25,0 } ,"Melee weapon damage "} // STUStatescriptActionPlayScript has scripts with melee weapon damage!
                 };
+
+        private object GetSTUDynValue(STUConfigVar v, ulong stu) {
+            if (stu == 0) return null;
+            foreach (var i in GetInstances<STUStatescriptActionSetVar>(stu)) {
+                if ((v as STUConfigVarDynamic).m_identifier == (i.m_out_Var as STUConfigVarDynamic).m_identifier)
+                    return i.m_value.GetType()?.GetField("m_value")?.GetValue(i.m_value);
+            }
+            return null;
+        }
+
+        private object GetSTUVarValue(STUConfigVar v, ulong stu, bool ignoreWeaponDefault = false) {
+            if (v.GetType().IsAssignableFrom(typeof(STUConfigVarDynamic))){
+                return GetSTUDynValue(v, stu);
+            }
+            else if (v.GetType().IsSubclassOf(typeof(STUConfigVarNumeric))) {
+                var num = v.GetType().GetField("m_value")?.GetValue(v);
+                return ignoreWeaponDefault ? (Convert.ToUInt16(num) == 999 ? null : num) : num;
+            }
+            else if (v.GetType() == (typeof(STU_919FD47C))) {
+                var expr = (v as STU_919FD47C)?.m_expression;
+                if (expr.m_D99EF254 != null)
+                    return string.Join("/", Array.ConvertAll<float, string>(expr.m_D99EF254, o => o.ToString()));
+                else if (expr.m_dynamicVars != null) {
+                    List<object> vals = new List<object>();
+                    foreach (var z in expr.m_dynamicVars) {
+                        vals.Add(GetSTUVarValue(z, stu));
+                    }
+                    return string.Join("/", vals.ConvertAll(Convert.ToString));
+                }
+            }
+            Log($"Unk STUConfigVar type: {v.GetType().FullName}");
+            //Log(v.GetType().FullName);
+            return null;
+        }
+
+        private void PrintSTUVar(string desc, STUConfigVar v, ulong stu = 0) {
+            if (v != null && desc != null) {
+                var val = GetSTUVarValue(v, stu, true);
+                if (val != null) {
+                    Log($"{desc}: {val}");
+                }
+            }
+        }
 
         //Dictionaries doesn't work properly with byte[] by default. Custom comparer must be implemented...
         //borrowed from https://stackoverflow.com/questions/1440392/use-byte-as-key-in-dictionary/30353296
@@ -140,7 +182,7 @@ namespace DataTool.ToolLogic.List {
                     continue;
                 heroes[key] = hero;
             }
-          
+
             return heroes;
         }
     }
