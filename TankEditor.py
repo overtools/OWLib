6892b3d4e3b78fc362f484ca98401b628f9c407d
prophetl33t <29117146+prophetl33t@users.noreply.github.com>
2022-04-01 23:18:08 +0700

    Add
    Add Overwatch 2 SUpport
    Add TankEditor
    Add PaladinsTool

---
+++ TankEditor.py
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+using System.Text;
+using TankLib.STU;
+
+namespace TankEdtior {
+    public class TankEdtior : ClassBuilder {
+    void Crash(){
+	return;
+    }
+    }
