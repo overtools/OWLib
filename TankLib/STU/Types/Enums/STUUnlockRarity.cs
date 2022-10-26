3c4a26943a4dfdf58dbf97f4ef004100338f8b0e
Js41637 <js41637@gmail.com>
2022-10-26 17:31:54 +1100

    add Mythic quality enum name

--- TankLib/STU/Types/Enums/STUUnlockRarity.cs
+++ TankLib/STU/Types/Enums/STUUnlockRarity.cs
@@ -10,7 +10,7 @@ namespace TankLib.STU.Types.Enums
         [STUField(0xC25BCFAD)] Rare = 0x1,
         [STUField(0xAF948D7A)] Epic = 0x2,
         [STUField(0x4585FFFF)] Legendary = 0x3,
-        [STUField(0xBD3BA67B)] xBD3BA67B = 0x4,
+        [STUField(0xBD3BA67B)] Mythic = 0x4,
         [STUField(0xEF68A54A)] xEF68A54A = 0x5,
         [STUField(0x3A3ADA03)] None = -1,
     }
