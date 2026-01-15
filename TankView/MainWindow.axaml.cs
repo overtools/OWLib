9ea1bc189a281c79076a31053686156eae61198f
Ada <ada@chronovore.dev>
2026-01-15 19:35:32 +0530

    tankview: sketch out navbar

--- TankView/MainWindow.axaml.cs
+++ TankView/MainWindow.axaml.cs
@@ -1,9 +1,12 @@
+using System.Reflection;
 using Avalonia.Controls;
+using TankLib;
 
 namespace TankView;
 
 public partial class MainWindow : Window {
 	public MainWindow() {
 		InitializeComponent();
+		Status.Text = $"{Assembly.GetExecutingAssembly().GetName().Name} v{Util.GetVersion(typeof(Program).Assembly)}";
 	}
 }
