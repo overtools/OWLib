overtools/OWLib

commit 3c9b223242fc26e3695dd81e2b89730b44c5bde0

fix ancient bugs in extracting as dds

TankLib/teTexture.cs
@@ -35,22 +35,22 @@ namespace TankLib {
                     Depth = 0,
                     MipmapCount = (uint)mips,
                     Format = TextureTypes.TextureType.Unknown.ToPixelFormat(),
-                    Caps1 = 0x1000,
+                    Caps1 = 0x1000, // DDSCAPS_TEXTURE - required
                     Caps2 = 0,
                     Caps3 = 0,
                     Caps4 = 0,
                     Reserved2 = 0
                 };
-                if (surfaces > 1 || IsArray) {
-                    ret.Caps1 = 0x8 | 0x1000;
+
+                if (surfaces > 1 || mips > 1 || IsArray || IsCubemap) {
+                    ret.Caps1 |= 0x8; // DDSCAPS_COMPLEX
                 }
-
-                if (IsCubemap) ret.Caps2 = 0xFE00;
-
-                // todo: wtf
-                if (MipCount > 1 && (PayloadCount == 1 || IsCubemap)) {
-                    ret.MipmapCount = MipCount;
-                    ret.Caps1 = 0x8 | 0x1000 | 0x400000;
+                if (mips > 1) {
+                    ret.Caps1 |= 0x400000; // DDSCAPS_MIPMAP
+                }
+                if (IsCubemap) {
+                    // DDSCAPS2_CUBEMAP_*
+                    ret.Caps2 |= 0xFE00;
                 }
 
                 return ret;
@@ -133,7 +133,7 @@ namespace TankLib {
             }
 
             Data = new byte[Header.DataSize];
-            reader.Read(Data, 0, (int)Header.DataSize);
+            reader.ReadExactly(Data);
         }
 
         public teResourceGUID GetPayloadGUID(ulong textureGUID, uint payloadIdx) {
@@ -266,7 +266,7 @@ namespace TankLib {
                         payload.SaveToDDSData(Header, ddsWriter);
                     }
                 } else {
-                    ddsWriter.Write(Data, 0, (int) Header.DataSize);
+                    ddsWriter.Write(Data, 0, Header.DataSize);
                 }
             }
         }

DataTool/SaveLogic/Combo.cs
@@ -776,7 +776,7 @@ public static class Combo {
             if (texture.Header.Is3D) return; // todo zingy - broken. 32DF4.004
 
             if (texture.PayloadRequired && texture.Payloads.Length > 1) {
-                for (uint i = (uint)texture.Payloads.Length-1u; i >= 1; i++) {
+                for (uint i = (uint)texture.Payloads.Length-1u; i >= 1; i--) {
                     using (var payloadStream = OpenFile(texture.GetPayloadGUID(textureGUID, i)))
                         texture.LoadPayload(payloadStream, i);
 

commit 878d9488ecaa2e251df1d825d63b1d308931f984

temp fix texture crash
caused by commit 3c9b2232 cause of Naraka texture

TankLib/teTexture.cs
@@ -132,7 +132,10 @@ namespace TankLib {
             }
 
             Data = new byte[Header.DataSize];
-            reader.ReadExactly(Data);
+            //reader.ReadExactly(Data);
+
+            // todo: one Naraka texture has invalid size and causes exception when using read exactly, needs investigation
+            reader.Read(Data, 0, (int)Header.DataSize);
         }
 
         public teResourceGUID GetPayloadGUID(ulong textureGUID, uint payloadIdx) {